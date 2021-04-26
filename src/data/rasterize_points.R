3d2260cd345660f17157a8aa6f8010ab4ebcd422
Daniel <dansmi@ceh.ac.uk>
2021-04-26 13:47:46 +0100

    New funcitonal script with better functions

--- src/data/rasterize_points.R
+++ src/data/rasterize_points.R
@@ -1,80 +1,176 @@
-# Points to Raster
+## A reformatted version and functional approach to "rasterize_points.R"
 
-library(raster)
+
+# Libraries ---------------------------------------------------------------
+
 library(tidyverse)
+library(raster)
 library(sf)
+library(rworldmap)
+library(inlmisc)
+library(cowplot)
 
-# the points
+# Functions ---------------------------------------------------------------
+
+# Get a worldmap shape layer and save it to a directory ----
+getworldmap <- function(res, dir) {
+  # Get the map
+  worldmap <- getMap(resolution = res)
+  # As sf object
+  worldmap <- st_as_sf(worldmap)
+  # Change the CRS to the European 3035 projection
+  worldmap <- st_transform(worldmap, 3035)
+  # Write as netCDF, append = F overwrites .nc files
+  st_write(worldmap, dir, append = FALSE)
+}
+
+# Get the Map
+getworldmap(res = "low", dir = "data/interim/worldmap_3035.nc")
+
+# Rasterize points file and get sampling effort for diff resolutions ----
+
+
+points2raster <- function(points, crs, res, method) {
+  
+  # # test block
+  # points = mosquito_points
+  # crs = 3035
+  # res = 10000 # 10km
+  # method = "count"
+
+  # Print
+  print("Please make sure to use a CRS that is projected in metres (m)")
+  # Get points in correct format and CRS
+  points <- st_as_sf(points)
+  points <- st_transform(points, crs = crs)
+  # Add an attribute for counting the points occurence
+  points$value <- 1
+  
+  # Generate a blank raster from those points
+  blank_raster <- raster(extent(points), res = res)
+  crs(blank_raster) <- 3035
+  proj4string(blank_raster) <- "+init=epsg:3035"
+  
+  # Do X dependent on input value
+  if (method == "count") {
+    # Count the points per geo unit (km on works)
+    raster <- rasterize(points, blank_raster, "value", fun = "count")
+  } else if (method == "log") {
+    raster <- rasterize(
+      points,
+      blank_raster,
+      "value",
+      fun = function(x, ...)
+        log(length(x))
+    )
+  } else {
+    stop("Method must be one of 'count' or 'log'")
+  }
+  print("The error is arising due to the transition to WKT projection definitions and the changes in GDAL and PROJ. Just ignore them.")
+  return(raster)
+}
+
+# count_10km <- points2raster(mosquito_points, 3035, 10000, "log")
+# plot(count_10km)
+
+
+# Convert any raster file to a shapefile; easy plotting in ggplot ----
+raster2shape <- function(raster) {
+  shp <- Grid2Polygons(raster)
+  shp <- st_as_sf(shp)
+  # Do we need to do this? Is CRS inherited through this func?
+  # shp <- st_transform(shp, crs = crs)
+  return(shp)
+}
+
+# Works!
+# raster2shape(count_10km) %>% plot()
+
+
+# Methods -----------------------------------------------------------------
+
+# load in mosquito points fiel
 mosquito_points <- st_read("data/interim/mosquito_points.shp")
 
-# A blank raster layer
-blank_raster <- raster(extent(mosquito_points),
-                       # 30 arc minutes
-                       res = (1/60)*30)
-                       # 2.5 arcminutes 
-                       # res = (1/60)*2.5)
+# Get log and count values for 20, 15, 10 and 5km density
+# of mosquito recorder effort
 
-# Add a value to the mosquito points (just one)
-mosquito_points$value <- 1
+# Resolutions in Km
+resolutions <- c(5, 10, 25, 50)
+# Convert m to km
+resolutions <- resolutions * 1000
 
-# A very brief count of points per geo unit...
-rsum <- rasterize(mosquito_points, blank_raster, "value", fun = "count")
-plot(rsum)
+# loop over list of res
+counts <- lapply(resolutions, function(x) {
+  raster <- points2raster(mosquito_points, 3035, x, method = "count")
+  shapes <- raster2shape(raster)
+})
 
-# Logged version of the values above...
-rlog <- rasterize(mosquito_points, blank_raster, "value", fun=function(x,...)log(length(x)))
-plot(rlog)
-
-## get a world map
-library(rworldmap)
-
-worldmap <- getMap(resolution = "low")
-worldmap <- st_as_sf(worldmap)
-
-# for converting my raster to a shape file
-library(inlmisc)
-
-# log vlaue plotting
-rlog_shape <- Grid2Polygons(rlog)
-rlog_shape <- st_as_sf(rlog_shape)
-st_crs(rlog_shape) <- st_crs(worldmap)
+logs <- lapply(resolutions, function(x) {
+  raster <- points2raster(mosquito_points, 3035, x, method = "log")
+  shapes <- raster2shape(raster)
+})
 
 
-logeffort <- ggplot() + 
-  geom_sf(data = worldmap, fill = "antiquewhite") + 
-  geom_sf(data = rlog_shape, aes(col = z, fill = z)) +
-  coord_sf(xlim = c(-20, 65), ylim = c(35, 73)) +
-  theme_bw() +
-  theme(panel.background = element_rect(fill = "aliceblue")) +
-  scale_color_viridis_c() +
-  scale_fill_viridis_c() +
-  labs(colour = "Log\nSamples", fill = "Log\nSamples") +
-  ggtitle("Logged VectorNET Sampling Effort")
+# Plots -------------------------------------------------------------------
 
+# Load shapefile
 
-# count plotting
-rsum_shape <- Grid2Polygons(rsum)
-rsum_shape <- st_as_sf(rsum_shape)
-st_crs(rsum_shape) <- st_crs(worldmap)
+worldmap <- st_read("data/interim/worldmap_3035.nc")
 
+# Raw count plots
+counts_plots <- list()
 
-raweffort <- ggplot() + 
-  geom_sf(data = worldmap, fill = "antiquewhite") + 
-  geom_sf(data = rsum_shape, aes(col = z, fill = z)) +
-  coord_sf(xlim = c(-20, 65), ylim = c(35, 73)) +
+for (i in seq_along(resolutions)) {
+  p = ggplot() +
+  geom_sf(data = worldmap, fill = "antiquewhite") +
+  geom_sf(data = counts[[i]], aes(col = z, fill = z)) +
+  coord_sf(xlim = c(1652265, 7567265), ylim = c(523584, 4878584)) +
   theme_bw() +
   theme(panel.background = element_rect(fill = "aliceblue")) +
   scale_color_viridis_c() +
   scale_fill_viridis_c() +
   labs(colour = "Raw\nSamples", fill = "Raw\nSamples") +
-  ggtitle("Raw VectorNET Sampling Effort")
+  ggtitle(paste0("Resolution: ", resolutions[i]/1000, "Km"))
 
-library(patchwork)
+  counts_plots[[i]] = p
+  }
 
-raweffort / logeffort
+# Save
+plot_grid(plotlist = counts_plots, nrow = 2)
+ggsave("reports/figures/RawSamplingEffort.png", width = 8.3, height = 11.7, dpi = 300)
 
-ggsave("reports/figures/SamplingEffort.pdf", width = 8.3, height = 11.7)
-ggsave("reports/figures/SamplingEffort.png", width = 8.3, height = 11.7, dpi = 300)
+# log plots
+log_plots <- list()
+
+for (i in seq_along(resolutions)) {
+  p = ggplot() +
+    geom_sf(data = worldmap, fill = "antiquewhite") +
+    geom_sf(data = logs[[i]], aes(col = z, fill = z)) +
+    coord_sf(xlim = c(1652265, 7567265), ylim = c(523584, 4878584)) +
+    theme_bw() +
+    theme(panel.background = element_rect(fill = "aliceblue")) +
+    scale_color_viridis_c() +
+    scale_fill_viridis_c() +
+    labs(colour = "Log\nSamples", fill = "Log\nSamples") +
+    ggtitle(paste0("Resolution: ", resolutions[i]/1000, "Km"))
+  
+  counts_plots[[i]] = p
+}
+
+# Save
+plot_grid(plotlist = counts_plots, nrow = 2)
+ggsave("reports/figures/LogSamplingEffort.png", width = 8.3, height = 11.7, dpi = 300)
+
+
+
+
+
+
+
+
+
+
 
 
 
